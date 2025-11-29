c7ced99161cd3868fde0d2275f3d02f9c9bb73a3
23f2000114 <23f2000114@ds.study.iitm.ac.in>
2025-11-29 10:47:00 +0530

    delete

--- LLMFiles/runner.py
+++
@@ -1,7 +0,0 @@
-
-import pandas as pd
-df = pd.read_csv("demo-audio-data.csv", header=None)
-cutoff = 49354
-filtered_df = df[df.iloc[:, 0] > cutoff]
-answer = filtered_df.iloc[:, 0].sum()
-print(answer)
